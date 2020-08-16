cb10d008540f066661fa855831b95bd559e98f69
luciaines91 <68884483+luciaines91@users.noreply.github.com>
2020-08-16 18:36:22 -0300

    validación de campos y redirección a index1

--- js/login.js
+++ js/login.js
@@ -1,6 +1,29 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
 
 });
+
+  function validar1(valor) {
+    if (valor == "") {
+      document.getElementById("error-nombre").innerHTML = "Ingrese usuario";
+    } else
+      document.getElementById("error-nombre").innerHTML = " ";
+  }
+
+  function validar2(valor2) {
+    if (valor2 == "") {
+      document.getElementById("error-contraseña").innerHTML = "Ingrese contraseña";
+    } else
+      document.getElementById("error-contraseña").innerHTML = " ";
+  }
+
+  function newDoc() {
+    var valor = document.getElementById("nombre").value
+    var valor2 = document.getElementById("contraseña").value
+    if (valor !== "" && valor2 !== "") {
+      document.location.href = "index1.html";
+    } else
+      document.getElementById("ok").innerHTML = "Revise los datos";
+  }
