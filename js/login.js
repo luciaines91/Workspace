c4bfa8755f96cc87b73fbead8556f3732a2a444e
luciaines91 <68884483+luciaines91@users.noreply.github.com>
2020-08-30 23:13:01 -0300

    Cambios entrega 2

--- js/login.js
+++ js/login.js
@@ -2,28 +2,36 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+  
 });
 
-  function validar1(valor) {
-    if (valor == "") {
-      document.getElementById("error-nombre").innerHTML = "Ingrese usuario";
-    } else
-      document.getElementById("error-nombre").innerHTML = " ";
-  }
+function validar1(valor) {
+  if (valor == "") {
+    document.getElementById("error-usuario").innerHTML = "Ingrese usuario";
+  } else
+    document.getElementById("error-usuario").innerHTML = " ";
+}
 
-  function validar2(valor2) {
-    if (valor2 == "") {
-      document.getElementById("error-contraseña").innerHTML = "Ingrese contraseña";
-    } else
-      document.getElementById("error-contraseña").innerHTML = " ";
-  }
+function validar2(valor2) {
+  if (valor2 == "") {
+    document.getElementById("error-contraseña").innerHTML = "Ingrese contraseña";
+  } else
+    document.getElementById("error-contraseña").innerHTML = " ";
+}
 
-  function newDoc() {
-    var valor = document.getElementById("nombre").value
-    var valor2 = document.getElementById("contraseña").value
-    if (valor !== "" && valor2 !== "") {
-      document.location.href = "index1.html";
-    } else
-      document.getElementById("ok").innerHTML = "Revise los datos";
-  }
+function newDoc() {
+  var valor = document.getElementById("usuario").value
+  var valor2 = document.getElementById("contraseña").value
+  var usuario = document.getElementById("usuario").value
+  var miStorage = window.sessionStorage;
+
+  if (valor !== "" && valor2 !== "") {
+    document.location.href = "index1.html";
+    miStorage.setItem("keyNombre", usuario);//Guardar variable usuario
+    document.getElementsById("keyNombre");
+  } else
+    document.getElementById("ok").innerHTML = "Revise los datos";
+}
+
+
+//alert(miStorage.getItem("keyNombre"))
