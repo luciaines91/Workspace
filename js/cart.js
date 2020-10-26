3ab18de647707673addfcd8ca231786289b7f70d
luciaines91 <68884483+luciaines91@users.noreply.github.com>
2020-10-25 23:50:27 -0300

    Entrega 6

--- js/cart.js
+++ js/cart.js
@@ -127,7 +127,7 @@ function validarpago() {
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(CART_INFO_TWO_PRODUCTS).then(function (resultObj) {
+    getJSONData(CART_INFO_URL).then(function (resultObj) {
         if (resultObj.status === "ok") {
             cartArray.articles = resultObj.data;
             //Muestro las categorías ordenadas
